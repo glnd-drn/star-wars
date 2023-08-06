f3784ca716752ca4bc39a7528d1c07230dc6d437
Glenda Durán <130619011+glnd-drn@users.noreply.github.com>
2023-08-06 03:26:30 +0000

    Navbar positions

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light fload-start">
+  <div className="container-fluid">
+    <a className="navbar-logo" href="#"></a>
+	<img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png"/>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+<div class="btn-group">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Favorites
+  </button>
+</div>
+  </div>
+</nav>
 	);
 };
